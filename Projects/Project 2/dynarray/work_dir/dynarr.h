9613c9cb0938ddb2100724605d0067bbc4a9eb6a
GodGetsGreens <r.wells6894@gmail.com>
2017-09-18 13:39:52 -0400

    Added function descriptions for all functions in dynarr.h

--- Projects/Project 2/dynarray/work_dir/dynarr.h
+++ Projects/Project 2/dynarray/work_dir/dynarr.h
@@ -1,3 +1,11 @@
+/*
+This is the header file that contains the class declarations and function
+definitions for RuntimeException, InvaildIndex, and most importantly
+the templated dynarr class and its functions.
+
+Author: Riley Wells
+*/
+
 #include <cassert>
 #include <iostream>
 #include <string>
@@ -39,6 +47,21 @@ class dynarr {
     // A equal to newA
 };
 
+
+
+/*
+
+dynarr(const dynarr<dynElem> &other)
+
+const dynarr<dynElem> &other - The reference to the dynamic array to be copied
+
+PURPOUSE:
+	This function takes in a reference to a dynamic array of type dynElem
+	and creates a deep copy of the refence which will be the newly created instance.
+
+RETURN: NONE
+
+*/
 template <class dynElem>
 dynarr<dynElem>::dynarr(const dynarr<dynElem> &other)
 {
@@ -50,10 +73,20 @@ dynarr<dynElem>::dynarr(const dynarr<dynElem> &other)
 		A[i] = other.A[i];
 	}
 
-	//return *this;
-
 }
 
+
+/*
+
+~dynarr()
+
+PURPOSE:
+	This is the destructor for the dynamic array object. It deletes the allocated
+	memory space and sets the A member of the instance to a nullptr.
+
+RETURN: NONE
+
+*/
 template <class dynElem>
 dynarr<dynElem>::~dynarr()
 {
@@ -63,6 +96,22 @@ dynarr<dynElem>::~dynarr()
 	
 }
 
+
+/*
+
+dynarr<dynElem>& operator=(const dynarr<dynElem> &other)
+
+PURPOSE:
+	The overlaoded assignment operator creates a deep copy of the dynamic array
+	passed by reference and sets the instance to be qual to other.
+
+RETURN:
+	If the two objects are the same, then the function returns a pointer to the instance
+
+	Else, it copies the information from other into the instance and retrusn the reference
+	to the instance.
+
+*/
 template <class dynElem>
 dynarr<dynElem> & dynarr<dynElem>::operator=( const dynarr<dynElem> &other)
 {
@@ -82,7 +131,25 @@ dynarr<dynElem> & dynarr<dynElem>::operator=( const dynarr<dynElem> &other)
 	return *this;
   
 }
-  
+
+
+/*
+
+dynElem& operator[](int ndx) throw(InvalidIndex)
+
+int ndx - The desired index of the dynamic array
+
+PRUPOSE: 
+	The overloaded [] operator takes an integer value ndx and returns
+	the dynElem at ndx of the array A
+
+RETURN:
+	If ndx < 0 or ndx >= the capacity of the dynamic array, an InvalidIndex
+	error is thrown.
+
+	Otherwise, the value at A[ndx] is returned.
+
+*/  
 template <class dynElem>
 dynElem & dynarr<dynElem>::operator[](int ndx) throw(InvalidIndex)
 {
@@ -94,6 +161,18 @@ dynElem & dynarr<dynElem>::operator[](int ndx) throw(InvalidIndex)
 
 }
 
+
+/*
+
+int getCapacity()
+
+PURPOSE:
+	This function returns the capacity of the instance object.
+
+RETURN: 
+	Returns the capacity of the instance
+
+*/
 template <class dynElem>
 int dynarr<dynElem>::getCapacity()
 {
@@ -101,7 +180,27 @@ int dynarr<dynElem>::getCapacity()
 	return capacity;
 
 }
-  
+
+
+/*
+
+void reserve(int newcap)
+
+PURPOSE:
+	If newcap <= capacity, the function does nothing.
+
+	If the capacity of the instance is 0, then create a new 
+	dynamically allocated array of type dynElem, make A point to 
+	the new array, and set acapacity to newcap.
+
+	If newcap > capacity, create a new dynamically allocated array of size newcap
+	and copy the values from A to the new array. Delete A, and set A to the new array.
+	Lastly, set capacity to newcap.
+
+RETURN:
+	NONE
+
+*/
 template <class dynElem>
 void dynarr<dynElem>::reserve(int newcap)
 {
